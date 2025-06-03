2410fd58ae38ad9a32ebecae9e3d0dd8512d2fc6
deepu0428 <adeepikareddy31@gmail.com>
2025-06-03 15:33:53 +0530

    Add files via upload

---
+++ 2.6.py
@@ -0,0 +1,6 @@
+mylist=intput("enter a list of numbers separated by space:")
+mylist=list(map(int,mylist.split()))
+num=0
+for num in mylist:
+    sum+=sum
+    print("the sum of the number is,"sum)
